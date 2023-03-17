57e9145c58631bf2d6392edcda56dc44926a57bb
Irha Ali <irhaali_97@hotmail.ca>
2023-03-17 15:16:19 -0400

    Add seeds

---
+++ server/db/seeds/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (name, email, password, postal_code)
+VALUES
+
+INSERT INTO blogs (user_id, title, image_url, content)
+VALUES
+
+INSERT INTO comments (user_id, comment)
+VALUES
+
+INSERT INTO articles (image_url, article_url)
+VALUES
+
+INSERT INTO saved_medications (user_id, name)
+VALUES
