1c87237c8181f0fa68f232a649b40992dc9fe2ce
Amaal Ali <amaalalikhadija@gmail.com>
2023-03-18 19:31:45 -0400

    add new journal log with counter and some css to delete and save button

--- client/src/components/MyJournal/index.js
+++ client/src/components/MyJournal/index.js
@@ -1,62 +1,74 @@
-import React, { useState } from 'react'
-import NotesList from './NotesList'
-import {nanoid} from 'nanoid'
-import profileImage from '../../assets/images/medicine.jpeg'
+import React, { useState } from 'react';
+import JournalList from './JournalList';
+import { nanoid } from 'nanoid';
+import profileImage from '../../assets/images/medicine.jpeg';
 
-import "../../styles/Journal.css"
+import "../../styles/Journal.css";
+import AddJournal from './AddJournal';
 const MyJournal = () => {
-  const[notes,setNotes]= useState([
-    { user:{
-      firstName: 'Bob' ,
-      lastName: 'Jacob',
-      image: profileImage
-    } ,
-    
-    text:"some text here! 1",
-    date:"06/02/1997"
-  },
-  { user:{
-    firstName:'Amaal'  ,
-    lastName: 'Ali',
-    image: profileImage
-  } ,
-    
-    text:"some text here! 2",
-    date:"06/02/1997"
-  },
-  { user:{
-    firstName: 'Irha',
-    lastName:'Ali' ,
-    image: profileImage
-  } ,
-  
-    text:"some text here! 3",
-    date:"06/02/1997"
-  },
-  { user:{
-    firstName: 'Maryan' ,
-    lastName: 'Ali',
-    image: profileImage
-  } ,
-   
-    text:"some text here! 4",
-    date:"06/02/1997"
-  },
-]);
-const AddNote =(text)=>{
-const date = new Date();
-const newNote={
-  text: text,
-  date:date.toLocaleDateString(),
-}
-const newNotes = [...notes,newNote];
-setNotes(newNotes);
-}
+  const [journals, setJournals] = useState([
+    {
+      user: {
+        firstName: 'Bob',
+        lastName: 'Jacob',
+        image: profileImage
+      },
+      id: nanoid(),
+      text: "some text here! 1",
+      date: "06/02/1997"
+    },
+    {
+      user: {
+        firstName: 'Amaal',
+        lastName: 'Ali',
+        image: profileImage
+      },
+      id: nanoid(),
+      text: "some text here! 2",
+      date: "06/02/1997"
+    },
+    {
+      user: {
+        firstName: 'Irha',
+        lastName: 'Ali',
+        image: profileImage
+      },
+      id: nanoid(),
+      text: "some text here! 3",
+      date: "06/02/1997"
+    },
+    {
+      user: {
+        firstName: 'Maryan',
+        lastName: 'Ali',
+        image: profileImage
+      },
+      id: nanoid(),
+      text: "some text here! 4",
+      date: "06/02/1997"
+    },
+  ]);
+  const AddJournal = (text) => {
+    const date = new Date();
+    const newJournal = {
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newJournals = [...journals, newJournal];
+    setJournals(newJournals);
+  };
+  const DeleteJournal = (id) => {
+    const newJournals = journals.filter((journal) => journal.id !== id);
+    setJournals(newJournals);
+  };
   return (
     <div className='container'>
-      <NotesList notes={notes} handleAddNote={AddNote}/>
+      <JournalList
+       journals={journals}
+        handleAddJournal={AddJournal}
+        handleDeleteJournal={DeleteJournal} />
     </div>
-  )
-}
+  );
+};
 
-export default MyJournal
+export default MyJournal;
