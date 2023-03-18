208c3c0eb89f3663adede95b2917f766fc171b9c
Irha Ali <irhaali_97@hotmail.ca>
2023-03-17 20:52:09 -0400

    Remove unwanted console.log

--- server/db/index.js
+++ server/db/index.js
@@ -7,7 +7,6 @@ const client = new pg.Client({
 
 client
   .connect()
-  .then(() => console.log("Connected to database"))
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = client;
