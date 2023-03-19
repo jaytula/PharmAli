0dc3882e1976fa7ea064fd180093430b5672065f
Irha Ali <irhaali_97@hotmail.ca>
2023-03-19 11:57:58 -0400

    Edit and remove journal queries and routes

---
+++ server/db/queries/remove-journal.js
@@ -0,0 +1,10 @@
+const getJournal = function(db, email) {
+  const queryParams = [email];
+  const queryString = `
+  DELETE FROM journals WHERE
+  user_id = (SELECT id FROM users WHERE email = $1)`
+  
+  return db.query(queryString, queryParams);
+};
+
+module.exports = { getJournal };
