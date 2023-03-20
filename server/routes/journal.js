6184a6af225bb6b44ac116d704458cd846140b44
Irha Ali <irhaali_97@hotmail.ca>
2023-03-20 18:54:25 -0400

    Fix add comment

--- server/routes/journal.js
+++ server/routes/journal.js
@@ -5,33 +5,25 @@ const removeJournal = require('../db/queries/remove-journal');
 // const { default: Journal } = require("../../client/src/components/MyJournal/Journal");
 
 module.exports = (db) => {
-  // To get all journals of a user
+  // To get all journal entries of a user
   router.get("/:id", (req, res) => {
     const id = req.url.replace('/', '')
-    console.log(id)
-    console.log("--------------")
     getJournal.getJournal(db, id)
       .then((journal) => {
         res.send({ journal: journal.rows });
       })
   });
   
-  // To edit or remove a journal of a user
+  // To remove a journal entry of a user
   router.post("/delete", (req, res) => {
-    // const { email, journal, edit_journal } = req.body;
     const journalId = Object.keys(req.body)[0]
-    // (edit_journal) ? editJournal.editJournal(db, journal, journalId) : removeJournal.removeJournal(db, journalId)
     removeJournal.removeJournal(db, journalId)
-    // console.log(Object.keys(req.body)[0])
   });
+
+  // To add a journal entry of a user
   router.post("/add", (req, res) => {
-    // const { email, journal, edit_journal } = req.body;
-    // const journalId = Object.keys(req.body)[0]
-    // (edit_journal) ? editJournal.editJournal(db, journal, journalId) : removeJournal.removeJournal(db, journalId)
-   
     addJournal.addJournal(db, req.body.user_id, req.body.text)
-   
-    // console.log(Object.keys(req.body)[0])
   });
+  
   return router;
 };
