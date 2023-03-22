da40e3b1ddd7c98671c974b6fa093cc3d93f802f
Irha Ali <irhaali_97@hotmail.ca>
2023-03-22 11:33:26 -0400

    Refractor saved medication route

--- server/routes/saved_medications.js
+++ server/routes/saved_medications.js
@@ -1,30 +1,28 @@
 const router = require("express").Router();
+const removeSavedMed = require("../db/queries/remove-saved-med");
+const addSavedMed = require("../db/queries/add-saved-med");
+const getSavedMed = require("../db/queries/get-saved-med");
 
 module.exports = db => {
+  // Get all or specific saved medication
   router.get("/:id", (request, response) => {
-    const queryParams = request.url.substring(1).split("&");
-    const queryString = (queryParams.length === 2) ? `SELECT * FROM saved_medications WHERE user_id = $1 AND drug_id = $2;` : `SELECT * FROM saved_medications WHERE user_id = $1;`
-
-    db.query(queryString, queryParams)
+    getSavedMed.getSavedMed(db, request.url.substring(1).split("&"))
       .then(({ rows: saved_medications }) => {
         response.json(saved_medications);
       })
   });
 
+  // Save a medication for a user
   router.post("/add", (request, response) => {
-    const queryParams = [request.body.user_id, request.body.drug_id]
-    const queryString = `INSERT INTO saved_medications (user_id, drug_id)
-    VALUES ($1, $2) RETURNING id`
-    db.query(queryString, queryParams)
+   addSavedMed.addSavedMed(db, request.body.user_id, request.body.drug_id)
       .then((data) => {
         response.json(data.rows[0]);
       });
   });
 
+  // Remove a saved medication for a user
   router.post("/remove", (request, response) => {
-    const queryParams = [request.body.favourite]
-    const queryString = `DELETE FROM saved_medications WHERE id = $1`
-    db.query(queryString, queryParams)
+    removeSavedMed.removeSavedMed(db, request.body.favourite)
       .then(() => {
         response.sendStatus(200);
       });
