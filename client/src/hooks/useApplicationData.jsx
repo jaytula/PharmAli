bc0990c74e78b6a9812aa355846d79cf4439c120
Irha Ali <irhaali_97@hotmail.ca>
2023-03-22 10:22:39 -0400

    Fix drug page

--- client/src/hooks/useApplicationData.jsx
+++ client/src/hooks/useApplicationData.jsx
@@ -1,18 +1,17 @@
 import axios from "axios";
 import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
+
 
 export default function useApplicationData() {
   const [menu, setMenu] = useState(false);
   const [drugContent, setDrugContent] = useState();
   const [user, setUser] = useState({});
   const [darkMode, setDarkMode] = useState(false);
+  // const navigate = useNavigate();
 
   const onSearchSubmit = (drug) => {
-    return Promise.all([
-      axios.get(`https://api.fda.gov/drug/label.json?search=description:${drug.name}`)
-    ]).then((data) => {
-      setDrugContent({ data, drug_id: drug.id });
-    })
+    // navigate(`/drugs/${drug.id}`);
   }
 
   const setCookie = (userInfo) => {
@@ -34,7 +33,7 @@ export default function useApplicationData() {
 
   const removeCookie = () => {
     return axios.post("/user/logout")
-      .then(() => { setUser("") })
+      .then(() => { setUser(null) })
   };
 
   const getCookie = () => {
