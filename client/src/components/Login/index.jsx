4c2e1984a4e2e2724115e4cd2f373fc47c678184
Amaal Ali <amaalalikhadija@gmail.com>
2023-03-23 00:48:51 -0400

    add final changes to login page

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -60,15 +60,24 @@ const Login = () => {
      <div class="login_container">
          <div class="login-content">
           
-      <h2>Login </h2>
+      <h1>LOGIN</h1>
       <form className="login-form" onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="placeyouremail@HERE.com" id="email" name="email" required/>
-        <label htmlFor="password">Password</label>
-        <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******" id="password" name="password" required/>
-        <Button children={LOGIN}/>
+        <label className='label-login' htmlFor="email">Email : </label>
+        <div class="form-group">
+        <input className='form-input' value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="placeyouremail@HERE.com" id="email" name="email" required/>
+        </div>
+        <div class="form-group">
+        <label className='label-login' htmlFor="password">Password : </label>
+        </div>
+        <div class="form-group">
+        <input className='form-input' value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******" id="password" name="password" required/>
+        </div>
+        <button className='login-button' children={LOGIN}></button>
       </form>
-      <button className="link-btn" onClick={() => navigate('/register')}>Dont have an account? Register</button>
+      <div className='signup'>
+
+      <button className="link-btn" onClick={() => navigate('/register')}>Dont have an account? Register HERE !</button>
+      </div>
     </div> 
     </div> 
     </div> 
