07ee1a2a685ebff09490e93fd90a05e0c2e8a7eb
Irha Ali <irhaali_97@hotmail.ca>
2023-03-23 20:03:30 -0400

    Move favourite drug list to App.jsx

--- client/src/components/MyDrugs/index.jsx
+++ client/src/components/MyDrugs/index.jsx
@@ -6,21 +6,21 @@ import DrugListItem from "../MyDrugItem";
 
 const MyDrugs = (props) => {
   const navigate = useNavigate();
-  const [drugs, setDrugs] = useState([]);
+  // const [drugs, setDrugs] = useState([]);
 
   useEffect(() => {
     if (props.user) {
       Promise.all([
         axios.get(`/favourite/${props.user}`),
       ]).then((data) => {
-        setDrugs(data[0].data)
+        props.setDrugs(data[0].data)
       })
     }
   }, [props.user]);
 
   return (
     <>
-      {drugs.map((drug) => (
+      {props.drugs.map((drug) => (
         <DrugListItem
           key={drug.drug_id}
           drug={drug}
