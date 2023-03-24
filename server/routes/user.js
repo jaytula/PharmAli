40bbf69b5ed1c8a2f6de97758f7015bb44d42b6a
Amaal Ali <amaalalikhadija@gmail.com>
2023-03-23 21:14:37 -0400

    add navbar changes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const getUser = require('../db/queries/get-user');
+const getUserById = require('../db/queries/get-user-id');
 const addUser = require('../db/queries/add-user');
 const bcrypt = require("bcryptjs");
 
@@ -7,20 +8,23 @@ const bcrypt = require("bcryptjs");
 module.exports = (db, cookieParams) => {
   // For getting login state when app is refreshed
   router.get("/", (req, res) => {
-        const user_id = req.signedCookies.name
-        res.send({ user_id });
+    const user_id = req.signedCookies.name;
+    getUserById.getUserById(db, user_id)
+      .then(({rows: user}) => {
+        res.send({ user });
+      });
   });
 
   // For logging in
   router.post("/login", (req, res) => {
-    const userInfo = { email: req.body.email }
+    const userInfo = { email: req.body.email };
     getUser.getUser(db, userInfo)
       .then((result) => {
         // If a user exists
         if (result.rows.length > 0) {
           // If password is correct
           if (bcrypt.compareSync(req.body.password, result.rows[0].password)) {
-            res.cookie('name', result.rows[0].id, cookieParams)
+            res.cookie('name', result.rows[0].id, cookieParams);
             message = { userInfo: result.rows[0] };
           } else {
             message = 'Incorrect password';
@@ -29,7 +33,7 @@ module.exports = (db, cookieParams) => {
           message = 'Account does not exist';
         }
         res.send({ message });
-      })
+      });
   });
 
   // For registering
@@ -40,18 +44,18 @@ module.exports = (db, cookieParams) => {
       email: req.body.email,
       password: bcrypt.hashSync(req.body.password, 10),
       postal_code: req.body.postalCode
-    }
+    };
     addUser.addUser(db, userInfo)
       .then((data) => {
         userInfo.id = data.rows[0].id;
         // If account doesn't exist with this email
-        res.cookie('name', userInfo.id, cookieParams)
+        res.cookie('name', userInfo.id, cookieParams);
         res.send({ message: { userInfo } });
       })
       .catch((err) => {
         // If account does exist with this email
         res.send({ message: err.detail });
-      })
+      });
   });
 
   // For logging out
