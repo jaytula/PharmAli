028e2e9334deba7164625e286b590493dff78f4a
Maryan <maryanay2000@gmail.com>
2023-03-24 18:30:52 -0400

    changed styling of homepage

--- client/src/components/Home/Hero.js
+++ client/src/components/Home/Hero.js
@@ -3,12 +3,14 @@ import "../../styles/hero.css";
 const Hero = ({imageSrc}) => {
   return (
     <div className="hero">
-      <div className="image_hero">
-      <img src={imageSrc} alt="Travel" className="hero__image"/>
+      <div className="hero-container">
+        <img src={imageSrc} alt="Travel" className="hero-image"/>
       </div>
-      <div className="overlay">
-    </div>
-      <h1 className="hero__title"> WHERE WE CARE ABOUT YOU 🤍</h1>
+
+      <div className="hero-subtitle">
+        <h1 className="hero-title"> BETTER DRUG CHOICES & CONFIDENCE STARTS HERE.</h1>
+        <p className="hero-paragraph">Pharmali provides accurate and independent information on more than 24,000 prescription drugs and substitutes over-the-counter medicines. This material is provided for educational purposes only and is not intended for medical advice, diagnosis, or treatment.</p>
+      </div>
     </div>
   )
 }
