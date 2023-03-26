b42ffe98e414e16accd8a07a88d1a89e14fa450a
Irha Ali <irhaali_97@hotmail.ca>
2023-03-26 17:57:48 -0400

    Clean up main files in client folder

--- client/src/components/Error.js
+++ client/src/components/Error.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import '../styles/login.css'
 
-const Error = (props) => {
+const Error = ({ message }) => {
 
   return (
     <main className="search__card search__card--error">
       <section className="search__error-message">
-        <h1 className="text--semi-bold"></h1>
-        <h3 className="text--light">{props.message}</h3>
+        <h3 className="text--light">{message}</h3>
       </section>
     </main>
   );
--- client/src/helpers/NavbarData.jsx
+++ client/src/helpers/NavbarData.jsx
@@ -1,62 +1,35 @@
-import React from 'react'
-import PersonIcon from '@mui/icons-material/Person';
-import HomeIcon from '@mui/icons-material/Home';
-import LocalPharmacyIcon from '@mui/icons-material/LocalPharmacy';
-import SearchIcon from '@mui/icons-material/Search';
-import BookIcon from '@mui/icons-material/Book';
-import CreateIcon from '@mui/icons-material/Create';
-import AutoStoriesIcon from '@mui/icons-material/AutoStories';
-import VaccinesIcon from '@mui/icons-material/Vaccines';
-
 const navbarData = (login) => {
 
   const navbarData = [
     {
       title: "SEARCH",
       url: "/search",
-      // icon: <SearchIcon color="primary" />,
-      // className: 'nav-text'
     },
     {
       title: "PHARMACY NEARBY",
       url: "/pharma",
-      // icon: <LocalPharmacyIcon color="primary" />,
-      // className: 'nav-text'
     },
     {
       title: "BLOG POSTS",
       url: "/blogs",
-      // icon: <BookIcon color="primary" />,
-      // className: 'nav-text'
     }
   ]
+
   // If logged in allow user to visit the additional pages available
   if (login) {
     
     navbarData.push(
-      // {
-      //   title: "MY BLOGS",
-      //   url: "/myblogs",
-      //   // icon: <AutoStoriesIcon color="primary" />,
-      //   // className: 'nav-text'
-      // },
       {
         title: "MY JOURNAL",
         url: "/myjournal",
-        // icon: <CreateIcon color="primary" />,
-        // className: 'nav-text'
       },
       {
         title: "MY MED LIST",
         url: "/mydrugs",
-        // icon: <VaccinesIcon color="primary" />,
-        // className: 'nav-text'
       },
       {
         title: "LOGOUT",
         url: "/logout",
-        // icon: <PersonIcon color="primary" />,
-        // className: 'nav-text'
       }
     )
   } else {
@@ -64,14 +37,10 @@ const navbarData = (login) => {
       {
         title: "LOGIN",
         url: "/login",
-        // icon: <PersonIcon color="primary" />,
-        // className: 'nav-text'
       },
       {
         title: "REGISTER",
         url: "/register",
-        // icon: <PersonIcon color="primary" />,
-        // className: 'nav-text'
       }
     )
   }
@@ -81,8 +50,6 @@ const navbarData = (login) => {
   navbarData.unshift({
     title: "HOME",
     url: "/",
-    // icon: <HomeIcon color="primary" />,
-    // className: 'nav-text'
   })
 
   return navbarData
