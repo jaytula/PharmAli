7f2296e2dcf6357f74ba9431e66f530e1cf33d2b
Irha Ali <irhaali_97@hotmail.ca>
2023-03-26 15:48:25 -0400

    Fix errors in pharmacy locator

--- client/src/components/PharmLocator/index.jsx
+++ client/src/components/PharmLocator/index.jsx
@@ -5,7 +5,7 @@ function PharmLocator() {
   return (
     <div className='pharmLocator'>
       <div className='pharmacy-map'>
-      <iframe src="https://my.atlistmaps.com/map/e2f947f5-e898-49e2-afc2-d6c7ac4d59fb?share=true" allow="geolocation 'self' https://my.atlistmaps.com" width="100%" height="1150px" frameborder="0" scrolling="no" allowfullscreen></iframe>
+        <iframe title="pharmacy_locator" src="https://my.atlistmaps.com/map/e2f947f5-e898-49e2-afc2-d6c7ac4d59fb?share=true" allow="geolocation 'self' https://my.atlistmaps.com" width="100%" height="1150px" frameBorder="0" scrolling="no" allowFullScreen></iframe>
       </div>
     </div>
   )
