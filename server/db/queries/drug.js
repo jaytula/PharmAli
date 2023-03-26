fec78b052f315038ada1e4050c4e915b9511e2e3
Irha Ali <irhaali_97@hotmail.ca>
2023-03-25 21:05:17 -0400

    Refractor drug code

---
+++ server/db/queries/drug.js
@@ -0,0 +1,21 @@
+
+const getDrug = function(db, params) {
+  let queryParams;
+  let queryString;
+
+  // If searching by name
+  if (isNaN(params)) {
+    queryParams = [`${params.toUpperCase()}%`];
+    queryString = `SELECT * FROM drugs WHERE name LIKE $1`
+  }
+
+  // If searching by id
+  if (!isNaN(params)) {
+    queryParams = [`${params}`];
+    queryString = `SELECT * FROM drugs WHERE id = $1`
+  }
+
+  return db.query(queryString, queryParams);
+};
+
+module.exports = { getDrug };
